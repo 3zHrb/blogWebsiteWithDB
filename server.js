090689cb3bcb38d9cc2eb3f2477ba756cf2786b7
Abdulaziz Alharbi <abdulazizalharbi@Abdulazizs-MacBook-Pro.local>
2020-09-15 23:51:45 +0300

    the mongoose is partially working, 1- we can check if we have data on the collection if not we will display empty space, 2- if we have data it will send the data to the home.ejs file loop through the array and take each object to access the data inside it and display it, 3- if we used the compose to post data to dataBase it will save the data we entered to the data base

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+var postTitle;
+var postBody;
+
+
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -14,28 +18,56 @@ app.listen(3000, function(){
   console.log("Session just started");
 });
 
+mongoose.connect("mongodb://localhost:27017/blogDataBase", {useNewUrlParser: true});
+var schema = {
+  title: String,
+  body: String
+};
+var Collection = mongoose.model("blogPosts", schema);
+
 app.get("/", function(req, res){
-  res.render("home");
+Collection.find({}, function(error, dataBack){
+if(error){
+  console.log("Error Occured while fetching the Collection");
+  return;}
+if(dataBack){
+  console.log("DataBack is: " + dataBack);
+  res.render("home", {dataBack: dataBack});
+};
+});
 });
 
 app.get("/aboutus", function(req,res){
-
 res.render("about");
-
 });
 
 app.get("/contactus", function(req, res){
-
 res.render("contact");
-
 });
 
 app.get("/home", function(req, res){
-
 res.redirect("/");
-
 });
 
 app.get("/compose", function(req, res){
-  res.render("compose");
+res.render("compose");
+});
+
+app.post("/posted", function(req, res){
+postTitle = req.body.postTitle;
+postBody = req.body.bodyTitle;
+
+var row = new Collection({
+  title: postTitle,
+  body: postBody
+});
+row.save(function(error, passed){
+  if(error){
+    console.log("error occured while saving data to Database");
+    return;
+  };
+if(passed){
+  res.redirect("/");
+};
+});
 });
